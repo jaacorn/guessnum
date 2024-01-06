ded6522c009c0663055f90841d3f2757dbf56866
jaacorn <jacorn812@gmail.com>
2024-01-06 18:06:54 -0500

    [1.2.1] - 2024-01-06 - Major Update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,22 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+
+## [1.2.1] - 2024-01-06 - Major Update
+### Added
+- Input validation
+- Hints
+
+### Changed
+- Re-wrote the whole thing
+
+### Fixed
+- Menu Navigation
+
+### Removed
+- Mid-game exit command
+
+
 ## [1.1.1] - 2017-12-23 - Official Alpha Release
 ### Added
 - Mid-game exit command
@@ -18,10 +34,12 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ### Fixed
 - Main menu input handling
 
+
 ## [0.0.2] - 2017-12-10
 ### Changed
 - Code optimizations
 - README.md updates
+
 
 ## [0.0.1] - 2017-12-10
 ### Added
--- guessmynum.py
+++ guessmynum.py
@@ -1,55 +1,94 @@
 # jaacorn - December 10, 2017 - Project Conception
 # jaacorn - December 10, 2017 - Ongoing Development
+# jaacorn - January 6, 2024 - Back in dev
 # guessmynum.py
 
 import random
 import sys
+import time
 
-# Begin program UI
-print("Welcome to \"Guess My Number\"!")
-print("")
-print("1 - Begin")
-print("2 - Help")
-print("3 - Exit")
+# GLOBAL VARIABLES
+rand_num = 0
+user_input = 0
+user_guess = 0
 
-mminput = input("::")
 
-if int(mminput) == 1:
-    num = random.randint(0,101)
-    print("Guess a number between 1 and 100")
-    print("")
+def safe_user_input(message):
     while True:
-        guessinput = input("::")
-        # check for 'quit game' command
-        if str(guessinput) == ("quit"):
-            print("")
-            print("Game quit")
-            sys.exit()
-        # check if above or below guess range
-        elif (int(guessinput) < 1 ) or (int(guessinput) > 100):
-            print("Guess too high or low")
-            print("Guess again")
-            print("")
-        # check if matches the randint
-        elif int(guessinput) == int(num):
-            print("Congratulations! You win!")
-            break
+        try:
+            userInput = int(input(message))       
+        except ValueError:
+            print("Not an valid number! Try again.")
+            continue
         else:
-            print("Incorrect guess")
-            # Uncomment below for debugging purposes
-            # print(num)
-            print("")
-    print("Game Over")
-elif int(mminput) == 2:
+            return userInput 
+            break
+
+
+def main():
+    global rand_num
+    global user_input
+    rand_num = random.randint(0,101)
+
+
+    print("Welcome to \"Guess My Number\"!")
     print("")
+    print("1 - Begin")
+    print("2 - Help")
+    print("3 - Exit")
+
+    user_input = safe_user_input(":: ")
+
+    if user_input == 1:
+        print("Guess a number between 1-100")
+        print("\n")
+        game()
+    elif user_input == 2:
+        game_help()
+    elif user_input == 3:
+        exit()
+    else:
+        print("Invalid input")
+        print("Please enter one of the above options")
+        time.sleep(3)
+        main()
+
+
+def game():
+    global user_guess
+    global rand_num
+    user_guess = safe_user_input(":: ")
+    if user_guess == rand_num:
+        correct_guess()
+    else:
+        incorrect_guess()
+
+
+def incorrect_guess():
+    print("\n")
+    print("Incorrect! Try again")
+    if user_guess > rand_num:
+        print("Hint: your guess was too high!")
+    else:
+        print("Hint: your guess was too low!")
+    game()
+
+
+def correct_guess():
+    print("CORRECT!")
+    print("You win!!!")
+    time.sleep(3)
+    main()
+
+
+def game_help():
+    print("\n")
     print("Help Menu:\n")
-    print("")
     print("   Guess My Number is a basic terminal game developed in Python.\n")
     print("The game is currently in alpha, and does not have a public release date.\n")
     print("All inquiries can be directed to jacorn812@gmail.com")
-elif int(mminput) == 3:
-    print("Goodbye")
-    sys.exit()
-else:
-    print("Invalid input")
-    print("Game closing")
+    user_input = input(":: ")
+    main()
+
+
+main()
